75bc462089792c910fb9e4cfce1c590e095e8391
Bima Saridjo <palessie27@gmail.com>
2020-06-11 16:42:13 -0400

    push stretch/

--- questions/39_getstudents/getstudents.js
+++ questions/39_getstudents/getstudents.js
@@ -4,8 +4,18 @@ const axios = require('./axios');
 //returning that specific student as specified in the test
 //* you don't need to NPM i axios for this test... Only work within this file and use promises
 
-const getStudent = () => {
+const getStudent = (name) => {
   //code in here
+  if(typeof name !== 'string'){ throw new Error('not a string'); } 
+
+  return axios.get('https://fullstack.com/api/cohort2004')
+  .then(res => { 
+    return { 
+      count: Object.keys(res.data).length,
+      student: Object.values(res.data).find(student => student.name === name) || 'no student found'
+    }
+  })
+  .catch(error => {throw([error])});
 };
 
 module.exports = { getStudent };
