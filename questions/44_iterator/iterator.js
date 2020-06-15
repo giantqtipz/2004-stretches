8f00d8d1587075db85d6ce5159a03b2418470eeb
Bima Saridjo <palessie27@gmail.com>
2020-06-15 18:52:49 -0400

    push stretch

--- questions/44_iterator/iterator.js
+++ questions/44_iterator/iterator.js
@@ -3,6 +3,24 @@
 
 class Iterator {
   //write code here
+  constructor(arr){
+    if(Array.isArray(arr)){
+      this.arr = arr;
+    } else {
+      throw new Error();
+    }
+  }
+
+  next(){
+    let done = false;
+    for(let i=0;i<this.arr.length;i++){
+      if(i === this.arr.length){ 
+        return {done: done, value: undefined}
+      }
+        return {done: done, value: this.arr[i]}
+      }
+  }
+
 }
 
 module.exports = { Iterator };
