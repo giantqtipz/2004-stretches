e98c6f62eff0c0e338b71037a6610b54a0b6d65c
Bima Saridjo <palessie27@gmail.com>
2020-06-19 15:17:53 -0400

    push workshop

--- questions/48_caeser_cypher/caeser_cypher.js
+++ questions/48_caeser_cypher/caeser_cypher.js
@@ -16,8 +16,24 @@
 // shift = 4
 // result = "csps123"
 
+// does not work when shift is greater than alphabet and numbers lengths...
 const encryptString = (str, shift) => {
   // YOUR CODE
+  if(!str.match(/^[a-z0-9]+$/i)) { throw new Error }
+
+    const caps = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'.split(''); 
+    const letters = 'abcdefghijklmnopqrstuvwxyz'.split('');
+    const numbers =  '123456789'.split('');
+  
+    const reduction = str.split('').reduce((a,b) => {
+      caps.includes(b) ? b = caps[caps.indexOf(b) + shift] || caps[shift]: null;
+      letters.includes(b) ? b = letters[letters.indexOf(b) + shift] || letters[shift] : null;
+      numbers.includes(b) ? b = numbers[numbers.indexOf(b) + shift] || numbers[shift] : null;
+      a.push(b);
+      return a;
+    }, [])
+  
+    return reduction.join('');
 };
 
 module.exports = { encryptString };
