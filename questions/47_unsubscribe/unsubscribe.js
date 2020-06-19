96828faa356746cc85d37482d67229e4582fe4ec
Bima Saridjo <palessie27@gmail.com>
2020-06-19 15:19:32 -0400

    push workshop

--- questions/47_unsubscribe/unsubscribe.js
+++ questions/47_unsubscribe/unsubscribe.js
@@ -2,6 +2,7 @@
 
 class Messenger {
   constructor(channel) {
+    if(typeof channel !== 'object') { throw Error('not an object')}
     this.channel = channel.name;
     this.listeners = {};
   }
@@ -9,9 +10,12 @@ class Messenger {
     this.listeners[type] = this.listeners[type] || [];
     return this.listeners[type];
   }
+
   subscribe(type, fn) {
-    this.getChannel(type).push(fn);
+    this.getChannel(type).push(fn)
+    return Object.keys(this.listeners).includes(type) ? () => this.listeners[type] = [] : null;
   }
+
   publish(type, msg) {
     this.getChannel(type).forEach((listener) =>
       listener(`${this.channel} - ${type} - ${msg}`)
