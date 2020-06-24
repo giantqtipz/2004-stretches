ed274e885139e2366de4b1fde2a5c9359804bd4b
Bima Saridjo <palessie27@gmail.com>
2020-06-24 18:55:22 -0400

    push stretch

--- questions/50_depthmap/depthmap.js
+++ questions/50_depthmap/depthmap.js
@@ -2,8 +2,25 @@
 //Write a function to recursively "dig" through this object installing a depth key along the way
 //return the object with it's depth indicated
 
-const recordDepth = () => {
+const recordDepth = (obj) => {
   //write code here
+  if(typeof obj !== 'object') {throw 'must be an object'}
+
+  let number = 0;
+  let newObj = {};
+  const recurse = (arg) => {
+    for(let key in arg){
+      if(typeof arg[key] === 'object'){
+        number++;
+        newObj = arg[key]
+        arg.depth = number;
+        return recordDepth(arg[key]);
+      } else {
+        return arg;
+      }
+    }
+  }
+  recurse(obj);
 };
 
 module.exports = { recordDepth };
