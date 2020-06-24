9742d139fcc51ad64b5b2b2775d15b5484c5cb13
Bima Saridjo <palessie27@gmail.com>
2020-06-23 23:22:17 -0400

    push workshop

--- questions/49_calc/calc.js
+++ questions/49_calc/calc.js
@@ -2,6 +2,26 @@
 
 class chainCalc {
   //write code here
+  constructor(num){
+    if(typeof num !== 'number') { throw Error() } 
+    this.result = num;
+  }
+  
+  add(arg){
+    this.result = this.result + arg;
+    return this; 
+  }
+
+  sub(arg){
+    this.result = this.result - arg;
+    return this; 
+  }
+
+  mul(arg){
+    this.result = this.result * arg;
+    return this; 
+  }
+
 }
 
 module.exports = { chainCalc };
