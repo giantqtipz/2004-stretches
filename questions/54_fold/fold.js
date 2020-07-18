bbce7c78f4dca2c51e391062e4824e2d21f5efba
Bima Saridjo <palessie27@gmail.com>
2020-07-18 09:23:58 -0400

    push stretch

--- questions/54_fold/fold.js
+++ questions/54_fold/fold.js
@@ -1,5 +1,22 @@
-const fold = () => {
+const fold = (arr) => { // oops.. it takes two arguments... 
   //YOUR CODE HERE
+  let result = [];
+
+  const recurse = (arg) => {
+    for(let i=0; i<arg.length;i++){
+      if(arg[i]){
+        result.push(arg[i] + arg[arg.length-1]);
+        arg.shift();
+        arg.pop();
+        console.log(result);
+        return recurse(arg);
+      } else {
+        return result;
+      }
+    }
+  }
+
+  return recurse(arr);
 };
 
 module.exports = fold;
